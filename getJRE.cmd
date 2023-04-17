a1eb4f478c547e9ce85f65be6c20961e457908d0
xitiomet <brian@openstatic.org>
2023-04-16 21:59:31 -0400

    Tweak to getJRE.cmd for cryptography req

--- getJRE.cmd
+++ getJRE.cmd
@@ -1,3 +1,3 @@
 @echo off
-jlink --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.security.jgss,java.sql,java.xml,org.graalvm.nativeimage.llvm,org.graalvm.truffle --output .\jre --strip-debug --compress 2 --no-header-files --no-man-pages
+jlink --add-modules java.base,java.desktop,java.logging,java.management,java.naming,java.security.jgss,jdk.crypto.ec,jdk.crypto.cryptoki,java.sql,java.xml,org.graalvm.nativeimage.llvm,org.graalvm.truffle --output .\jre --strip-debug --compress 2 --no-header-files --no-man-pages
 pause
